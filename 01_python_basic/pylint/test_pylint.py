36b8df237829e8f966908a5fcea811057d03a23a
bankze34 <bankze34@gmail.com>
2021-05-25 14:08:44 +0900

    fixed for comparing code(diff)

--- 01_python_basic/pylint/test_pylint.py
+++ 01_python_basic/pylint/test_pylint.py
@@ -1,22 +1,30 @@
-entry =[
-    ("김사랑",70,75),
-    ("이하늬",60,65),  
-    ("이지은",80,85),
-    ("서윤희",90,95),
-    ("김은정",85,95)]  
-sum = []
-def makeSum(entry, # ignore_01 : 괄호 안의 인수는 세로로 정렬되지 않거나 내어 쓰기를 사용하지 않아야 한다
-  sum) :
-    if not entry:
-      print("entry list is empty")
-        for i in range(0,5) :
-            sum.append(entry[i][1]+entry[i][2])
-        sum.sort(reverse=True) # ignore_02 : 연산자 앞뒤 공백이 있어야 한다
-        
-        print ("진",list(filter(lambda x:x[1]+x[2]==sum[0],entry)))
-				# ignore_03 : print문에서 print와 괄호 사이의 공백은 없어야 한다
-        print ("선",list(filter(lambda x:x[1]+x[2]==sum[1],entry)))
-        print ("미",list(filter(lambda x:x[1]+x[2]==sum[ 2 ],entry)))
-				# ignore_04 : 대괄호([]) 안의 공백은 없어야 한다
+"""
+    미스코리아 상위 3명을 가려 진선미를 선출한다.
+"""
+entry = [
+    ("김사랑", 70, 75),
+    ("이하늬", 60, 65),
+    ("이지은", 80, 85),
+    ("서윤희", 90, 95),
+    ("김은정", 85, 95)]
 
-makeSum(entry,sum)
+sum = []
+
+def make_sum(inner_entry, inner_sum) :
+    """
+        inner_entry {list} : 명단
+        inner_sum {list} : 진, 선, 미를 가려 저장할 명단
+    """
+    if not inner_entry:
+        print("entry list is empty")
+    else:
+        for i in range(0, 5) :
+            inner_sum.append(entry[i][1] + entry[i][2])
+
+        inner_sum.sort(reverse = True)
+
+        print("진", list(filter(lambda x : x[1] + x[2] == inner_sum[0], entry)))
+        print("선", list(filter(lambda x : x[1] + x[2] == inner_sum[1], entry)))
+        print("미", list(filter(lambda x : x[1] + x[2] == inner_sum[2], entry)))
+
+make_sum(entry, sum)
